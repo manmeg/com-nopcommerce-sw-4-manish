239f651cb113df4701948af66fdef66af0e223ef
manmeg <manmeg151@gmail.com>
2023-07-11 20:08:08 +0100

    Homework Done

---
+++ src/test/java/com/nopcommerce/demo/testng/TestNGDemo.java
@@ -0,0 +1,29 @@
+package com.nopcommerce.demo.testng;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGDemo
+{
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("I am in before method");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("I am in after method");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("I am in test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("I am in test2");
+    }
+}
